f5270f990e2d5cd4bbfb92509420671158664a9b
Vijay Sridhar, Ted Campbell <ted.campbell.email@gmail.com>
2015-06-23 17:58:55 -0400

    Initial commit test, re do with files from xp64

---
+++ Math.svc.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFServiceIISJuly31
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Math : IMath
+    {
+        public string GetData()
+        {
+            return string.Format("You entered the Service");
+        }
+
+       
+    }
+}
